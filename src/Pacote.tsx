34508f968941f8b6d8fa4d473ae62be40955a9d1
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2025-01-27 20:47:55 -0300

    muda o espaço da memória para o react atualizar o estado

--- src/Pacote.tsx
+++ src/Pacote.tsx
@@ -2,20 +2,36 @@ import { useState } from "react";
 import "./Pacote.css";
 
 export default function Pacote() {
-  const [largura, setLargura] = useState(5);
-  const [altura, setAltura] = useState(5);
-  const [peso, setPeso] = useState(200);
-  const [profundidade, setProfundidade] = useState(5);
+    const[pacote, setPacote]= useState({
+        altura:7,
+        largura:7,
+        profundidade:7,
+        peso:7
+    });
+
+
+//   const [largura, setLargura] = useState(5);
+//   const [altura, setAltura] = useState(5);
+//   const [peso, setPeso] = useState(200);
+//   const [profundidade, setProfundidade] = useState(5);
 
   function onChange(e:React.ChangeEvent<HTMLInputElement>) {
     if (e.target.name === "largura") {
-      setLargura(e.target.valueAsNumber);
+        const novoPacote = structuredClone(pacote);
+        novoPacote.largura = e.target.valueAsNumber;
+        setPacote(novoPacote);
     } else if (e.target.name === "altura") {
-      setAltura(e.target.valueAsNumber);
+        const novoPacote = structuredClone(pacote);
+        novoPacote.altura = e.target.valueAsNumber;
+        setPacote(novoPacote);
     } else if (e.target.name === "profundidade") {
-      setProfundidade(e.target.valueAsNumber);
+        const novoPacote = structuredClone(pacote);
+        novoPacote.profundidade = e.target.valueAsNumber;
+        setPacote(novoPacote);
     } else if (e.target.name === "peso") {
-      setPeso(e.target.valueAsNumber);
+        const novoPacote = structuredClone(pacote);
+        novoPacote.peso = e.target.valueAsNumber;
+        setPacote(novoPacote);
     }
   }
   return (
@@ -25,7 +41,7 @@ export default function Pacote() {
         {
           <input
             type="number"
-            value={largura}
+            value={pacote.largura}
             min="5"
             step={"1"}
             name="largura"
@@ -38,7 +54,7 @@ export default function Pacote() {
         {
           <input
             type="number"
-            value={altura}
+            value={pacote.altura}
             min="5"
             step={"1"}
             name="altura"
@@ -51,7 +67,7 @@ export default function Pacote() {
         {
           <input
             type="number"
-            value={profundidade}
+            value={pacote.profundidade}
             min="5"
             step={"1"}
             name="profundidade"
@@ -64,7 +80,7 @@ export default function Pacote() {
         {
           <input
             type="number"
-            value={peso}
+            value={pacote.peso}
             min="5"
             step={"1"}
             name="peso"
@@ -74,7 +90,7 @@ export default function Pacote() {
       </dd>
       <dt>Custo</dt>
       <dd>
-        {(largura * altura * profundidade * peso * 0.0005).toLocaleString(
+        {(pacote.largura * pacote.altura * pacote.profundidade * pacote.peso * 0.0005).toLocaleString(
           "pt-BR",
           { style: "currency", currency: "BRL" }
         )}
