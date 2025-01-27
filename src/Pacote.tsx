c685832cbed25e04ad37452136d12b9e0aaefdc7
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2025-01-27 20:12:08 -0300

    adiciona novos estados

--- src/Pacote.tsx
+++ src/Pacote.tsx
@@ -4,6 +4,10 @@ import "./Pacote.css"
 export default function Pacote()
 {
     const [largura, setLargura] = useState(5);
+    const [altura, setAltura] = useState(5);
+    const [peso, setPeso] = useState(5);
+    const [profundidade, setProfundidade] = useState(5);
+
 
     return <dl className="pacote">
         <dt>Largura (cm)</dt>
@@ -11,11 +15,17 @@ export default function Pacote()
             setLargura(evento.target.valueAsNumber)
         }}/>}</dd>
         <dt>Altura (cm)</dt>
-        <dd>10</dd>
+        <dd>{<input type="number" value={altura} min="5" step={"1"} onChange={(evento)=>{
+            setAltura(evento.target.valueAsNumber)
+        }}/>}</dd>
         <dt>Profundidade (cm)</dt>
-        <dd>10</dd>
+        <dd>{<input type="number" value={profundidade} min="5" step={"1"} onChange={(evento)=>{
+            setProfundidade(evento.target.valueAsNumber)
+        }}/>}</dd>
         <dt>Peso (g)</dt>
-        <dd>10</dd>
+        <dd>{<input type="number" value={peso} min="5" step={"1"} onChange={(evento)=>{
+            setPeso(evento.target.valueAsNumber)
+        }}/>}</dd>
         <dt>Custo</dt>
         <dd>{(largura*0.50).toLocaleString("pt-BR", {style:"currency", currency:"BRL"})}</dd>
     </dl>
