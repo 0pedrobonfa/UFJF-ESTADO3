2186e90883e6eccd53870e984a5934adccba0c76
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2025-02-03 19:37:22 -0300

    fica em dia com a aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
+
+import { useState } from 'react';
 import './App.css'
-import Pacote from './Pacote'
+import Pacote, { PacoteType } from "./Pacote.tsx";
+import { PacoteChaveType } from './Pacote.tsx';
 
 function App() {
 
+  const [pacotes, setPacotes] = useState <Array<PacoteType>> 
+  ([
+    {
+      id: "p001",
+      largura:30,
+      altura:30,
+      profundidade:30,
+      peso:200,
+    },
+    {
+      id: "p002",
+      largura:30,
+      altura:30,
+      profundidade:30,
+      peso:200,
+    },
+    {
+      id: "p003",
+      largura:30,
+      altura:30,
+      profundidade:30,
+      peso:200,
+    },
+  ]);
+
+  function onEditarPacote(pacote: PacoteType, chave: PacoteChaveType, valor: number) : void
+  {
+    const novoPacote = {...pacote};
+    novoPacote[chave] = valor;
+    setPacotes((pacotes.map(p=>p!==pacote?p:novoPacote)));
+  }
+
+  function excluirPacote(pacote:PacoteType)
+  {
+    setPacotes(pacotes.filter(p=>p.id!==pacote.id));
+  }
+
   return (
     <>
-      <Pacote></Pacote>
+    {pacotes.map(p=>
+      <Pacote key={p.id} pacote={p} onEditarPacote={onEditarPacote} onExcluirPacote={excluirPacote}/>
+    )}
     </>
   )
 }
--- src/Pacote.tsx
+++ src/Pacote.tsx
@@ -1,100 +1,62 @@
-import { useState } from "react";
 import "./Pacote.css";
 
-export default function Pacote() {
-    const[pacote, setPacote]= useState({
-        altura:7,
-        largura:7,
-        profundidade:7,
-        peso:7
-    });
+export type PacoteChaveType = "altura" | "largura" | "profundidade" | "peso";
 
+export type PacoteType = {
+    id: string;
+    altura: number;
+    largura: number;
+    profundidade: number;
+    peso: number;
+};
 
-//   const [largura, setLargura] = useState(5);
-//   const [altura, setAltura] = useState(5);
-//   const [peso, setPeso] = useState(200);
-//   const [profundidade, setProfundidade] = useState(5);
+export type PacotePropsType = {
+    pacote: PacoteType;
+    onEditarPacote: (pacote: PacoteType, chave: PacoteChaveType, valor: number) => void
+    onExcluirPacote: (pacote: PacoteType) => void;
+}
 
-  function onChange(e:React.ChangeEvent<HTMLInputElement>) {
-    if (e.target.name === "largura") {
-        const novoPacote = structuredClone(pacote);
-        novoPacote.largura = e.target.valueAsNumber;
-        setPacote(novoPacote);
-    } else if (e.target.name === "altura") {
-        const novoPacote = structuredClone(pacote);
-        novoPacote.altura = e.target.valueAsNumber;
-        setPacote(novoPacote);
-    } else if (e.target.name === "profundidade") {
-        const novoPacote = structuredClone(pacote);
-        novoPacote.profundidade = e.target.valueAsNumber;
-        setPacote(novoPacote);
-    } else if (e.target.name === "peso") {
-        const novoPacote = structuredClone(pacote);
-        novoPacote.peso = e.target.valueAsNumber;
-        setPacote(novoPacote);
+export default function Pacote({ pacote, onEditarPacote, onExcluirPacote}: PacotePropsType) {
+
+    // FUNÇÃO QUE ALTERA VALOR DOS ELEMENTOS
+    function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const chave = e.target.name as PacoteChaveType;
+        const valor = e.target.valueAsNumber;
+        onEditarPacote(pacote, chave, valor);
+            
     }
-  }
-  return (
-    <dl className="pacote">
-      <dt>Largura (cm)</dt>
-      <dd>
-        {
-          <input
-            type="number"
-            value={pacote.largura}
-            min="5"
-            step={"1"}
-            name="largura"
-            onChange={onChange}
-          />
-        }
-      </dd>
-      <dt>Altura (cm)</dt>
-      <dd>
-        {
-          <input
-            type="number"
-            value={pacote.altura}
-            min="5"
-            step={"1"}
-            name="altura"
-            onChange={onChange}
-          />
-        }
-      </dd>
-      <dt>Profundidade (cm)</dt>
-      <dd>
-        {
-          <input
-            type="number"
-            value={pacote.profundidade}
-            min="5"
-            step={"1"}
-            name="profundidade"
-            onChange={onChange}
-          />
-        }
-      </dd>
-      <dt>Peso (g)</dt>
-      <dd>
-        {
-          <input
-            type="number"
-            value={pacote.peso}
-            min="5"
-            step={"1"}
-            name="peso"
-            onChange={onChange}
-          />
-        }
-      </dd>
-      <dt>Custo</dt>
-      <dd>
-        {(pacote.largura * pacote.altura * pacote.profundidade * pacote.peso * 0.0005).toLocaleString(
-          "pt-BR",
-          { style: "currency", currency: "BRL" }
-        )}
-      </dd>
-    </dl>
-  );
+
+    return <dl className="pacote">
+
+        {/* LARGURA */}
+        <dt>Largura (cm)</dt>
+        <dd> 
+        <input type="number" value={pacote.largura} min="5" step="1" name="largura" onChange={onChange}/></dd>
+
+        {/* ALTURA */}
+        <dt>Altura (cm)</dt>
+        <dd> 
+        <input type="number" value={pacote.altura} min="5" step="1" name="altura" onChange={onChange}/></dd>
+
+        {/* PROFUNDIDADE */}
+        <dt>Profundidade (cm)</dt>
+        <dd> 
+        <input type="number" value={pacote.profundidade} min="5" step="1" name="profundidade" onChange={onChange}/></dd>
+
+        {/* PESO */}
+        <dt>Peso (g)</dt>
+        <dd> 
+        <input type="number" value={pacote.peso} min="200" step="1" name="peso" onChange={onChange}/></dd>
+
+        {/* CUSTO */}
+        <dt>Custo</dt>
+        <dd>{(pacote.largura*pacote.altura*pacote.profundidade*pacote.peso * 0.00005)
+        .toLocaleString("pt-BR",{
+            style:"currency",currency:"BRL"
+        })}</dd>
+
+        <dt>Ações</dt>
+        <dd><button onClick={()=>{onExcluirPacote(pacote)}}>Excluir</button></dd>
+
+    </dl>;
 }
